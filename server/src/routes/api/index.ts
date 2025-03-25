45c96998838244b3055d2de1b711a3de574d234f
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-03-24 22:12:47 -0700

    Type definition to import express module and use it in the application, being necessary install it with npm i --save-dev @types/express

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+//install express to stop error: 
+//npm i --save-dev @types/express
+
 const router = Router();
 
 import weatherRoutes from './weatherRoutes.js';
